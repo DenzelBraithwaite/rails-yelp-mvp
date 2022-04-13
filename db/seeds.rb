c47f2a186e6de2dffff3ae3be81b530d094a1d6f
Denzel Braithwaite <braithwaite.d@hotmail.com>
2022-04-12 20:56:55 -0400

    created models, working on controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# id: nil, name: nil, address: nil, phone_number: nil, category: nil, created_at: nil, updated_at: nil>
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+puts "Creating restaurants..."
+
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: categories.sample
+  )
+  puts "Created #{restaurant.name}"
+end
+
+puts "Finished!"
