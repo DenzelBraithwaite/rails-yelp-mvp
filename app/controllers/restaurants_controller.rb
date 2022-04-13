774db176ef5aedceaf8d56ddc84e5b475aa48861
Denzel Braithwaite <braithwaite.d@hotmail.com>
2022-04-12 21:25:37 -0400

    added reviews controller but didn't touch it yet, working on completing a form next for the new.html

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,3 +1,24 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    # Finish create method, hint: use render
+    @restaurant = Restaurant.new(restaurant_params)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants, except: [:edit, :update, :destroy] do
-    resources :reviews, except: [:edit, :update, :destroy]
+    resources :reviews, only: [:new, :create]
   end
 end
+
+# watch video part where he ensures review references restaurant
